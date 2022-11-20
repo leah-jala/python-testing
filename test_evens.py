020d6e3c8b2185c3e5f2056515993c17c9d83f43
leah-jala <leahlafond@protonmail.com>
2022-11-20 16:03:50 +0000

    Add evens example and tests

---
+++ test_evens.py
@@ -0,0 +1,21 @@
+import unittest
+from evens import even_number_of_evens
+
+# To make use of unittest, our class needs to 
+# inherit from the unittest.TestCase class
+
+class TestEvens(unittest.TestCase):
+    
+    def test_throws_error_if_value_passed_in_is_not_list(self):
+        self.assertRaises(TypeError, even_number_of_evens, 4)
+    
+
+    def test_values_in_list(self):
+        self.assertEqual(even_number_of_evens([]), False)
+        self.assertEqual(even_number_of_evens([2, 4]), True)
+        self.assertEqual(even_number_of_evens([2]), False)
+        self.assertEqual(even_number_of_evens([1, 3, 5]), False)
+
+
+if __name__ == '__main__':
+    unittest.main()
